97f6fce71c7ecce7c0b41ed5c286dfb4660bbf62
Sayed-cpu <elgoharysayed410@gmail.com>
2025-04-23 05:16:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/homescreen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      initialRoute:Homescreen.routeName ,
+      routes: {
+        Homescreen.routeName: (context) => Homescreen(),
+      },
+    );
+  }
+}
